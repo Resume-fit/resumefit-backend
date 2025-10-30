193a377b1c2f33684b60144f4f52bc17c13c5090
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-30 21:07:29 +0900

    feat: resume mapper

---
+++ src/main/java/com/resumefit/resumefit_backend/domain/resume/mapper/ResumeMapper.java
@@ -0,0 +1,16 @@
+package com.resumefit.resumefit_backend.domain.resume.mapper;
+
+import com.resumefit.resumefit_backend.domain.resume.dto.ResumePostDto;
+import com.resumefit.resumefit_backend.domain.resume.dto.ResumeSummaryDto;
+import com.resumefit.resumefit_backend.domain.resume.entity.Resume;
+import java.util.List;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ResumeMapper {
+
+    // Resume toResumeEntity(ResumePostDto resumePostDto);
+    ResumeSummaryDto toResumeSummaryDto(Resume resume);
+    List<ResumeSummaryDto> toResumeSummaryDtoList(List<Resume> resumeList);
+
+}
