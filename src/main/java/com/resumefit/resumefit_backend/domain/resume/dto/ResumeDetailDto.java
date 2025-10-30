ed7665e2e3fa1206fa08d54f98aa9a22301c1052
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-30 21:07:07 +0900

    feat: resume dto for get specific resume

---
+++ src/main/java/com/resumefit/resumefit_backend/domain/resume/dto/ResumeDetailDto.java
@@ -0,0 +1,21 @@
+package com.resumefit.resumefit_backend.domain.resume.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ResumeDetailDto {
+
+    private String title;
+    private String fileKey; // S3에 저장된 파일의 고유 키
+    private LocalDateTime createdAt; // 생성 일시
+    private LocalDateTime updatedAt; // 최종 수정 일시
+    private String pdfViewUrl;
+
+}
