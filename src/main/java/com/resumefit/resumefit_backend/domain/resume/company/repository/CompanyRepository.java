baa64d8e16922a504c2c38973f8febcd70fc869a
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-11-08 19:10:14 +0900

    remove

--- src/main/java/com/resumefit/resumefit_backend/domain/resume/company/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.resumefit.resumefit_backend.domain.resume.company.repository;
-
-import com.resumefit.resumefit_backend.domain.resume.company.entity.Company;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {}
