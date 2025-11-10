5ea651644c2fa05681f5acded36071b3719a48d5
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-11-10 13:00:33 +0900

    feat: implement review submission functionality with Review entity and service

---
+++ src/main/java/com/resumefit/resumefit_backend/domain/resume/controller/ResumeController.java
@@ -0,0 +1,82 @@
+package com.resumefit.resumefit_backend.domain.resume.controller;
+
+import com.resumefit.resumefit_backend.domain.resume.dto.ResumeDetailDto;
+import com.resumefit.resumefit_backend.domain.resume.dto.ResumePostDto;
+import com.resumefit.resumefit_backend.domain.resume.dto.ResumeSummaryDto;
+import com.resumefit.resumefit_backend.domain.resume.service.ResumeService;
+import com.resumefit.resumefit_backend.domain.review.dto.ReviewRequestDto;
+import com.resumefit.resumefit_backend.domain.review.service.ReviewService;
+import com.resumefit.resumefit_backend.domain.user.dto.CustomUserDetails;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = " 이력서 api", description = "이력서와 관련된 API들입니다.")
+@RequestMapping("/api/resumes")
+public class ResumeController {
+
+    private final ResumeService resumeService;
+    private final ReviewService reviewService;
+
+    @Operation(summary = "이력서 저장", description = "사용자의 이력서를 저장합니다.")
+    @PostMapping
+    public ResponseEntity<Void> saveResume(
+            @RequestBody ResumePostDto resumePostDto,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        resumeService.processResumePost(resumePostDto, userDetails);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "모든 이력서 조회", description = "사용자의 모든 이력서를 조회합니다.")
+    @GetMapping
+    ResponseEntity<List<ResumeSummaryDto>> getAllMyResume(
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(resumeService.getAllMyResume(userDetails));
+    }
+
+    @Operation(summary = "이력서 상세 조회", description = "이력서 ID를 통해 특정 이력서를 조회합니다.")
+    @GetMapping("/{resumeId}") // 예시: GET /api/resumes/{resumeId}
+    public ResponseEntity<ResumeDetailDto> getResumeById(
+            @PathVariable("resumeId") Long resumeId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(resumeService.getResume(resumeId, userDetails));
+    }
+
+    @Operation(summary = "이력서 삭제", description = "이력서 ID를 통해 특정 이력서를 삭제합니다.")
+    @DeleteMapping("/{resumeId}")
+    ResponseEntity<Void> deleteResume(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable("resumeId") Long resumeId) {
+        resumeService.deleteResume(resumeId, userDetails);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "이력서 후기 제출", description = "특정 매칭에 대한 후기를 제출합니다.")
+    @DeleteMapping("/{resumeId}/review")
+    ResponseEntity<Void> submitReview(
+        @PathVariable("resumeId") Long resumeId,
+        @Valid @RequestBody ReviewRequestDto reviewRequestDto,
+        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        reviewService.submitReview(resumeId, reviewRequestDto, userDetails);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
